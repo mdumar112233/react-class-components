e51620aa6fac78d0c27dc00cb12b28a901180175
md umar faruk <mdumar11233>
2021-04-20 22:52:23 +0600

    added new code

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,10 +6,16 @@ class Home extends Component {
     constructor(props, context) {
         super(props, context);
         this.state = {
-            users:[]
+            users:[],
+            cart: []
         };
     }
     
+    handleAddToCard(user){
+        const newCart = [...this.state.cart];
+        this.setState({cart: newCart});
+        console.log(user);
+    }
 
     componentDidMount() {
         fetch('https://jsonplaceholder.typicode.com/users')
@@ -20,8 +26,9 @@ class Home extends Component {
         return (
             <div>
                 <h2>{this.state.users.length}</h2>
+                <h3>Users added in the Cart{this.state.cart.length}</h3>
                 {
-                    this.state.users.map(user => <Users user={user}></Users>)
+                    this.state.users.map(user => <Users user={user}handleAddToCard={this.handleAddToCard}></Users>)
                 }
             </div>
         );
--- src/Components/Users.js
+++ src/Components/Users.js
@@ -5,6 +5,7 @@ class Users extends Component {
         return (
             <div>
                 <h4>This is {this.props.user.name}</h4>
+                <button onClick={this.props.handleAddToCard(this.props.user)}>add me</button>
             </div>
         );
     }
